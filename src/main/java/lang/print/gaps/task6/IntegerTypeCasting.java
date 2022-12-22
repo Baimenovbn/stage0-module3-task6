80088913ad5928363d6c31db566e3489e6f60aa0
Baimenovbn <baibn@mail.ru>
2022-12-22 23:41:48 +0600

    feat: finish task

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,9 +3,8 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000;
-        int second;
+        int second = (int) first;
 
-        second = (int) first;
         System.out.println(first);
         System.out.println(second);
     }
